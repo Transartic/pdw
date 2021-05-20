2f1af69b07cbd7fc4cd8c978c4a05348389b845f
Alexander Zinn <alexzinnde@gmail.com>
2021-05-20 18:49:49 -0400

    Semantic UI working

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,22 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'resolve-url-loader'],
+        include: [
+          path.join(__dirname, '../client', 'src'),
+          /node_modules/,
+        ],
+      },
+      {
+        test: /\.s[a|c]ss$/,
+        loader: 'sass-loader!style-loader!css-loader',
+      },
+      {
+        test: /\.(jpg|png|gif|jpeg|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000',
+      },
     ],
   },
   resolve: {
