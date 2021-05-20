da35ce0dc2ba97f4830863661157941ae6597b43
Alexander Zinn <alexzinnde@gmail.com>
2021-05-20 18:11:11 -0400

    Basic file structure

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Pucci</h1>
+      </div>
+    );
+  }
+}
+export default App;
