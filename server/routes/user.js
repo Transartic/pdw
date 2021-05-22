5ae7f0ef5a5713f5881caef590f610eea5c15e72
Alexander Zinn <alexzinnde@gmail.com>
2021-05-22 13:36:55 -0400

    Create login route that returns JWT

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,11 @@
 /* eslint-disable camelcase */
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 const { User } = require('../database');
+
 // Add a new user to the DB
 const checkNewUserInput = (req, res, next) => {
   const { body } = req;
@@ -20,7 +23,7 @@ const checkNewUserInput = (req, res, next) => {
   }
   res.status(400).send('missing required field');
 };
-router.post('/signup', checkNewUserInput, (req, res) => {
+router.post('/signup', checkNewUserInput, async (req, res) => {
   const {
     username,
     password,
@@ -38,25 +41,55 @@ router.post('/signup', checkNewUserInput, (req, res) => {
     services,
     certifications,
   } = req.body;
-  User.create({
-    username,
-    password,
-    email,
-    firstName: first_name,
-    lastName: last_name,
-    address1,
-    address2,
-    city,
-    state,
-    zipcode,
-    dogName: dog_name,
-    description,
-    userType: user_type,
-    services,
-    certifications,
-  })
-    .then((user) => res.send(user))
-    .catch((err) => res.status(500).send(err));
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    User.create({
+      username,
+      password: hashedPassword,
+      email,
+      firstName: first_name,
+      lastName: last_name,
+      address1,
+      address2,
+      city,
+      state,
+      zipcode,
+      dogName: dog_name,
+      description,
+      userType: user_type,
+      services,
+      certifications,
+    })
+      .then((user) => res.status(201).send(user))
+      .catch((err) => res.status(500).send(err));
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// login route
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ where: { username } });
+    // if username is not in the DB return 404
+    if (user === null) {
+      res.status(404).send('Username Not Found');
+      return;
+    }
+
+    // check that password is correct
+    if (await bcrypt.compare(password, user.password)) {
+      const accessToken = jwt.sign({ user_id: user.id }, process.env.ACCESS_TOKEN_SECRET);
+
+      res.json({ accessToken });
+    }
+
+    res.status(400).send('Username or Password incorrect');
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 module.exports = router;
