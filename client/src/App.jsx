5528b33027ee7c763f81a503878fd8a4add6ad9b
Joseph McFatridge <josephdanielmcfatridge@gmail.com>
2021-05-22 14:29:46 -0500

    Worked on creating the main css for the dog owner and walker pages. The general layout has been made and may require a few adjustments moving forward.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,8 +8,33 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="logged-in-profile">
         <h1>Pucci</h1>
+
+        <div className="profile-container">
+          <div className="profile-picture">This is the profile-picture class</div>
+          <div className="username">Thie is username class</div>
+          <div className="profile-button-right">This is profile-buttons class</div>
+          <div className="profile-info">This is profile-info class</div>
+        </div>
+
+        <div className="posts-schedule-walks-container">
+          <div className="auction-posts">
+            <div className="post">This is the post class</div>
+            <div className="post">This is the post class</div>
+            <div className="post">This is the post class</div>
+          </div>
+
+          <div className="schedule">This is the schedule class</div>
+
+          <div className="walks-container">
+            <div className="next-walk">This is the next-walk class</div>
+            <div className="checklist">This is the checklist class</div>
+            <div className="previous-walk">This is the previous-walk class</div>
+          </div>
+        </div>
+
+        <div className="area-map">This is the area-map class</div>
       </div>
     );
   }
