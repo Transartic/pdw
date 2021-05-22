d4f04714788d7ea67a5955aae50829ca481bfd49
Joseph McFatridge <josephdanielmcfatridge@gmail.com>
2021-05-22 14:48:36 -0500

    Fixed sizing issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+// eslint-disable-next-line import/extensions
+import UserProfile from './UserProfile.jsx';
 
 class App extends Component {
   constructor() {
@@ -8,33 +10,8 @@ class App extends Component {
 
   render() {
     return (
-      <div className="logged-in-profile">
-        <h1>Pucci</h1>
-
-        <div className="profile-container">
-          <div className="profile-picture">This is the profile-picture class</div>
-          <div className="username">Thie is username class</div>
-          <div className="profile-button-right">This is profile-buttons class</div>
-          <div className="profile-info">This is profile-info class</div>
-        </div>
-
-        <div className="posts-schedule-walks-container">
-          <div className="auction-posts">
-            <div className="post">This is the post class</div>
-            <div className="post">This is the post class</div>
-            <div className="post">This is the post class</div>
-          </div>
-
-          <div className="schedule">This is the schedule class</div>
-
-          <div className="walks-container">
-            <div className="next-walk">This is the next-walk class</div>
-            <div className="checklist">This is the checklist class</div>
-            <div className="previous-walk">This is the previous-walk class</div>
-          </div>
-        </div>
-
-        <div className="area-map">This is the area-map class</div>
+      <div className="app">
+        <UserProfile />
       </div>
     );
   }
