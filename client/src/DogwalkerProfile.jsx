6391007d17793f5fdc206f9956913795a8d843d9
tishvo <tishvo@gmail.com>
2021-05-24 11:45:11 -0400

    dogwalker profile component created and routes created

---
+++ client/src/DogwalkerProfile.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class DogwalkerProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Dogwalker Profile goes here</h1>
+      </div>
+    );
+  }
+}
+
+export default DogwalkerProfile;
