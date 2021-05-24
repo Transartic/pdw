35fb51a01825fbfca5ad615e7798785720914d70
Joseph McFatridge <josephdanielmcfatridge@gmail.com>
2021-05-24 18:19:11 -0500

    Created a modal for the view bids button

--- client/src/BidPost.jsx
+++ client/src/BidPost.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ViewBidsModal from './ViewBidsModal.jsx';
 
 const BidPost = ({ page }) => {
   let profileBid;
@@ -13,7 +14,9 @@ const BidPost = ({ page }) => {
           <div className="post-username">Username</div>
           <div className="post-info">Post Info kajhfiouahfiouahdiouahiduhaiwudhiauwhdiuawhdiauhdiuahiwduhaiudhaiuwhdiauwhdiauhwdiuahwdiuahwdiuahwdiuawhiduahiduahwiduahwiduhawiudhaiwudhiauwhdiaudhiauhdiauwhdiauhdiawuhdih</div>
         </div>
-        <button className="post-button owner" type="submit">View &lsquo;x&lsquo; Bids</button>
+        <div className="post-button owner">
+          <ViewBidsModal />
+        </div>
       </div>
     );
   } else if (page === 'walker') {
@@ -28,20 +31,6 @@ const BidPost = ({ page }) => {
           <div className="post-info">Post Info kajhfiouahfiouahdiouahiduhaiwudhiauwhdiuawhdiauhdiuahiwduhaiudhaiuwhdiauwhdiauhwdiuahwdiuahwdiuahwdiuawhiduahiduahwiduahwiduhawiudhaiwudhiauwhdiaudhiauhdiauwhdiauhdiawuhdih</div>
         </div>
         <div className="bid-status">Pending Review</div>
-      </div>
-    );
-  } else {
-    profileBid = (
-      <div className="post-body">
-        <div className="post-date-time">
-          <div className="post-date">Date</div>
-          <div className="post-time">Time</div>
-        </div>
-        <div className="post-details">
-          <div className="post-username">Username</div>
-          <div className="post-info">Post Info kajhfiouahfiouahdiouahiduhaiwudhiauwhdiuawhdiauhdiuahiwduhaiudhaiuwhdiauwhdiauhwdiuahwdiuahwdiuahwdiuawhiduahiduahwiduahwiduhawiudhaiwudhiauwhdiaudhiauhdiauwhdiauhdiawuhdih</div>
-        </div>
-        <button className="post-button walker" type="submit">Place a Bid</button>
       </div>
     );
   }
