ece11a4d3ce90d94bbf6825df67c93e822e2a072
Jake Nalls <jakenalls@gmail.com>
2021-05-24 17:26:50 -0400

    accepted DB changes

--- server/database/index.js
+++ server/database/index.js
@@ -1,7 +1,6 @@
 const db = require('./connection');
 
 const User = require('./models/user');
-<<<<<<< HEAD
 const Review = require('./models/review');
 const Bid = require('./models/bid');
 
@@ -15,15 +14,3 @@ module.exports = {
   Review,
   Bid,
 };
-=======
-const Post = require('./models/post');
-
-User.hasMany(Post);
-Post.belongsTo(User);
-
-User.sync();
-Post.sync();
-
-module.exports.User = User;
-module.exports.Post = Post;
->>>>>>> b24006ba265d91d5859ff4112f14342776e90c17
