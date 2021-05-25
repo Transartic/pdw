0e03212a4de6be9383211df2c54f656df1759a3f
Alexander Zinn <alexzinnde@gmail.com>
2021-05-25 14:31:17 -0400

    automatically login user when signing up

--- server/middleware/authenticateToken.js
+++ server/middleware/authenticateToken.js
@@ -2,12 +2,14 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
+
   if (!token) {
     res.sendStatus(401).send('authorization token missing');
     return;
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
     if (err) return res.sendStatus(403);
+    console.log('JWT: ', data);
     req.userId = data.user_id;
     next();
   });
